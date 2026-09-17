0032334fa20139791219a4595c06531598bdf0b1
logeshDE <logeshwararavichandran811@gmail.com>
2026-09-17 13:37:01 +0530

    Add Cloudinary to installed apps

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,6 +13,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+        'cloudinary_storage',
+        'cloudinary',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'dashboard.apps.DashboardConfig',
